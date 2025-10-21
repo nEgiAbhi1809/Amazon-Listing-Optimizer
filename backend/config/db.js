efbb8119c6509bf62eb1465ce5dffda7c4733bdd
Abhishek Singh Negi <creatorlearner108@gmail.com>
2025-10-21 12:16:02 +0530

    UPDATED DB.JS

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,14 +3,13 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-
-
 export const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
+  process.env.DB_NAME,       // database name
+  process.env.DB_USER,       // username
+  process.env.DB_PASSWORD,   // password
   {
     host: process.env.DB_HOST,
+    port: process.env.DB_PORT || 3306, // port should be inside options
     dialect: "mysql",
     logging: false,
   }
